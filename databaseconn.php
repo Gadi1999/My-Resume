c3739938f385e883e817b2936112d99804b15592
Jesudanam Gadi <jesudhanam1999@gmail.com>
2024-10-17 16:15:31 +0000

    Update databaseconn.php

--- databaseconn.php
+++ databaseconn.php
@@ -1,5 +1,5 @@
     <?php
-      $servername = "4.240.55.215:3306"; // or your IP address without the port
+      $servername = "4.240.55.215:3306"; // IP address (without the port in this property)
       $username = "root";
       $password = "Gadi@1999";
       $dbname = "learncoding";
